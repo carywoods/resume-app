d54b151d93bd560dd8b484895949385932fab659
carywoods <cary@indybiosystems.com>
2022-02-12 16:55:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,7 +117,7 @@ txt('**Certification** (Google Cloud Digital Leadership), *Certificate ID*, NZWH
 st.markdown('''
 ''')
  
-with :
+with st.echo('Done'):
 
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
