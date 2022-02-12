aa9f83839534cb982c22841b42a890f82d8b532f
carywoods <cary@indybiosystems.com>
2022-02-12 16:10:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
 from PIL import Image
 
@@ -215,3 +219,30 @@ txt2('Scopus', 'http://www.scopus.com/authid/detail.url?authorId=12039071300')
 txt2('ResearcherID', 'http://www.researcherid.com/rid/F-1021-2010')
 txt2('ResearchGate', 'https://www.researchgate.net/profile/Chanin_Nantasenamat')
 txt2('Publons', 'https://publons.com/a/303133/')
+
+
+
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
+    data = []
+
+    points_per_turn = total_points / num_turns
+
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
+
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
+
+    
+    
+    
